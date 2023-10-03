078daf0a1ca0a37c4ff3f3787a22e2e87343bb71
Julian Simon <juedsay@gmail.com>
2023-10-03 12:56:55 -0300

    modificación de uitesting con pruebas de selectores

--- tests/uitesting.spec.ts
+++ tests/uitesting.spec.ts
@@ -1,9 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
-  await page.goto('http://uitestingplayground.com/');
-  await page.getByRole('link', { name: 'Resources' }).click();
-  await page.getByRole('link', { name: 'Home' }).click();
-  await page.getByRole('link', { name: 'Click' }).click();
-  await page.getByRole('button', { name: 'Button That Ignores DOM Click Event' }).click();
+   await page.goto('http://uitestingplayground.com/');
+   
+   await page.locator('#title').click();//Selector de ID
+   await page.locator('.alert').click();//Selector de Clase
+   await page.locator('a.navbar-brand').click();//Selector de etiqueta, combinado con una clase para ser más específico
+
+  // await page.getByRole('link', { name: 'Resources' }).click();
+  // await page.getByRole('link', { name: 'Home' }).click();
+  // await page.getByRole('link', { name: 'Click' }).click();
+  // await page.getByRole('button', { name: 'Button That Ignores DOM Click Event' }).click();
 });
