4ba98d55cb187c298be28d141bb4f4afeb4c319b
Julian Simón <juedsay@gmail.com>
2023-10-04 04:14:42 -0300

    avance en desafio

--- tests/tiendaonline.spec.ts
+++ tests/tiendaonline.spec.ts
@@ -3,19 +3,26 @@ import { test, expect } from '@playwright/test';
 test('add product to cart',async ({ page }) => {
 
     //1)Lauch browser and navigate to url: https://automationexercise.com/
-    await page.goto('https://automationexercise.com');
+    await page.goto('https://automationexercise.com/products');
 
-    //2)hover on firs product:
-    await page.locator('img[src="/get_product_picture/1"]').hover();
-
-    //Close the add:
-    if(page.url() === 'https://automationexercise.com/#google_vignette'){
-        await page.frameLocator('#aswift_5').frameLocator('internal:attr=[title="Advertisement"i]').getByRole('button', { name: 'Close ad' }).click();
+     //Close the add:
+    if(await page.locator('//*[@id="dismiss-button"]/div/span').xpalocator('iframe[name="ad_iframe"]').getByLabel('Close ad').isVisible()){
+        await page.locator('//*[@id="dismiss-button"]/div/span').locator('iframe[name="ad_iframe"]').getByLabel('Close ad').click();
     };
+    
+    //2)hover on firs product:    
+    await page.locator('img[src="/get_product_picture/1"]').hover();
     
     //3) click on button for more details of the first product:
     await page.locator('a[href="/product_details/1"]').click();
+
+    //Close the add:
+    if(await page.frameLocator('iframe[name="aswift_5"]').frameLocator('iframe[name="ad_iframe"]').getByLabel('Close ad').isVisible()){
+        await page.frameLocator('iframe[name="aswift_5"]').frameLocator('iframe[name="ad_iframe"]').getByLabel('Close ad').click();
+    };
+
     await expect(page).toHaveURL('https://automationexercise.com/product_details/1');
+    
 
     //4) Add change de value of quantity the products from 1 to 3
     await page.locator('#quantity').fill('3');
